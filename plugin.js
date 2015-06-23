2cdc15e9fcaac3104f4f54e0abe1c70369ae7068
Elze Kool <info@kooldevelopment.nl>
2015-06-23 20:41:58 +0200

    Whoops.. used tabs :(

--- plugin.js
+++ plugin.js
@@ -11,50 +11,50 @@
  **/
  ;(function(undefined) {
 
-	/**
-	 * Find onChange event to replace
-	 **/
-	var findOnChangeToReplace = function(editor, dialogName, definition, elements, parent_path)
+    /**
+     * Find onChange event to replace
+     **/
+    var findOnChangeToReplace = function(editor, dialogName, definition, elements, parent_path)
     {
         var element,
-			path;
+            path;
 
         // If dialog doesn't have any elements skip it
-		if ( !elements || !elements.length ) {
-			return;
+        if ( !elements || !elements.length ) {
+            return;
         }
 
-		// Go trough elements
-		for( var i = elements.length; i--; ) {
-		
-			element = elements[ i ];
-			path = element.id ? (parent_path + ':' + element.id) : parent_path;
-			
+        // Go trough elements
+        for( var i = elements.length; i--; ) {
+        
+            element = elements[ i ];
+            path = element.id ? (parent_path + ':' + element.id) : parent_path;
+            
             // Go deeper if layout element
-			if (element.type == 'hbox' || element.type == 'vbox' || element.type == 'fieldset') {
-				findOnChangeToReplace( editor, dialogName, definition, element.children, path );
+            if (element.type == 'hbox' || element.type == 'vbox' || element.type == 'fieldset') {
+                findOnChangeToReplace( editor, dialogName, definition, element.children, path );
             }
-			
-			// Check if we have the correct field
-			if (path !== 'info:txtUrl' || element.type !== 'text' || element.onChange === undefined) {
-				continue;
-			}
-			
-			// Replace the onChange function
-			var orgOnChange = element.onChange;
-			element.onChange = function() {
-			
-				// Set the internaly used flag dontResetSize
-				// normaly this is reset to false after updating
-				this.getDialog().dontResetSize = true;
-				
-				// Apply the normal function
-				orgOnChange.apply(this);	
-		
-			}
-			
-		}	
-	}
+            
+            // Check if we have the correct field
+            if (path !== 'info:txtUrl' || element.type !== 'text' || element.onChange === undefined) {
+                continue;
+            }
+            
+            // Replace the onChange function
+            var orgOnChange = element.onChange;
+            element.onChange = function() {
+            
+                // Set the internaly used flag dontResetSize
+                // normaly this is reset to false after updating
+                this.getDialog().dontResetSize = true;
+                
+                // Apply the normal function
+                orgOnChange.apply(this);    
+        
+            }
+            
+        }    
+    }
 
     // Init plugin
     CKEDITOR.plugins.add( 'no_image_size_reset', {
@@ -62,32 +62,32 @@
         }
     });
 
-	// Add dialogDefinition observer to CKEditor
-    CKEDITOR.on( 'dialogDefinition', function(e) {	
-	
-		var definition = e.data.definition,
-			name = e.data.name,
-			element;
-			
-		// Check if image plugin is enabled, if not we don't
-		// have anything to do
-		if ( !e.editor.plugins.image )
-			return;
+    // Add dialogDefinition observer to CKEditor
+    CKEDITOR.on( 'dialogDefinition', function(e) {    
+    
+        var definition = e.data.definition,
+            name = e.data.name,
+            element;
+            
+        // Check if image plugin is enabled, if not we don't
+        // have anything to do
+        if ( !e.editor.plugins.image )
+            return;
 
-		// Check if we are dealing with image dialog
-		if (name !== 'image' || definition.dialog.dontResetSize !== undefined) {
-			return;
-		}
-		
-		// Start scan for definitions
-		for ( var i = 0; i < definition.contents.length; ++i ) {
-			if ((element = definition.contents[i])) {
-				if (element.id === 'info') {			
-					findOnChangeToReplace( e.editor, e.data.name, definition, element.elements, 'info' );
-				}
-			}
-		}
+        // Check if we are dealing with image dialog
+        if (name !== 'image' || definition.dialog.dontResetSize !== undefined) {
+            return;
+        }
+        
+        // Start scan for definitions
+        for ( var i = 0; i < definition.contents.length; ++i ) {
+            if ((element = definition.contents[i])) {
+                if (element.id === 'info') {            
+                    findOnChangeToReplace( e.editor, e.data.name, definition, element.elements, 'info' );
+                }
+            }
+        }
 
-	});
+    });
 
 })();
